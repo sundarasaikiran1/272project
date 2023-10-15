059e6a75a32b0d10c7f6188105901579dedcc115
sundarasaikiran <117646749+sundarasaikiran1@users.noreply.github.com>
2023-10-15 00:42:52 -0700

    Add files via upload

---
+++ Saikiran PHP/api/services.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Our Services - Exquisite Motors</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+    <header>
+        <h1>Our Services</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="services">
+        <h2>What We Offer</h2>
+        <ul>
+            <li>Wide Selection of Luxury Cars</li>
+            <li>Expert Advice and Consultation</li>
+            <li>Vehicle Financing Options</li>
+            <li>Car Maintenance and Servicing</li>
+        </ul>
+    </section>
+
+    <!-- Additional content for the services page goes here -->
+
+    <footer>
+    &copy; 2023 Exquisite Motors. All rights reserved.
+    </footer>
+</body>
+</html>
