3663c719f6443cc49d3ec4c6f94ef6b7314f5a8c
sundarasaikiran <117646749+sundarasaikiran1@users.noreply.github.com>
2023-10-15 00:49:26 -0700

    Add files via upload

---
+++ api/contact.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Us - Mobile Devices Inc.</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+    <header>
+        <h1>Contact Us</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="contact-form">
+        <h2>Contact Mobile Devices Inc.</h2>
+        <p>
+            Do you have any questions or need assistance? Feel free to reach out to us using the contact information below:
+        </p>
+        <?php
+	if (file_exists(_DIR_ . '/../api/contacts.txt')) {
+            $lines = file(_DIR_ . '/../api/contacts.txt');
+            foreach ($lines as $line) {
+                $info = explode(': ', $line);
+                $label = $info[0];
+                $value = $info[1];
+                echo "<p><strong>$label:</strong> $value</p>";
+            }
+        } else {
+            echo "<p>Contact information not available.</p>";
+        }
+        ?>
+    </section>
+
+    <footer>
+        &copy; 2023 Mobile Devices Inc. All rights reserved.
+    </footer>
+</body>
+</html>
