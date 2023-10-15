5e06c3ba561375e43f1d48b0fe9c6b02d50d12bb
sundarasaikiran <117646749+sundarasaikiran1@users.noreply.github.com>
2023-10-15 00:48:53 -0700

    Create about.php

---
+++ api/about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us - Mobile Devices Inc.</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+    <header>
+        <h1>About Mobile Devices Inc.</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="about">
+        <h2>Who We Are</h2>
+        <p>
+            Welcome to Mobile Devices Inc., your one-stop destination for the latest mobile technology. We pride ourselves on offering a wide range of high-quality mobile devices and accessories to enhance your digital lifestyle.
+        </p>
+        <p>
+            At Mobile Devices Inc., we believe that technology should be accessible to everyone. Our team is dedicated to providing top-notch products and delivering excellent customer service. We're here to help you find the perfect mobile solutions for your needs, whether it's the latest smartphone, stylish accessories, or expert technical support.
+        </p>
+        <p>
+            Join us in exploring the exciting world of mobile devices and experience the future in your hands. We look forward to serving you and helping you stay connected in style.
+        </p>
+    </section>
+
+    <!-- Additional content for the About page goes here -->
+
+    <footer>
+        &copy; 2023 Mobile Devices Inc. All rights reserved.
+    </footer>
+</body>
+</html>
